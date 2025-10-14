5a803436674a526227b9daf834810f29f0f83b05
Lukas Luemers <lukas.luemers@gdata.de>
2025-10-14 18:04:07 +0200

    added tech stack section images

--- src/app/tech-stack/tech-stack.component.ts
+++ src/app/tech-stack/tech-stack.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-tech-stack',
-  imports: [],
+  imports: [MatIcon, MatButtonModule],
   templateUrl: './tech-stack.component.html',
-  styleUrl: './tech-stack.component.scss'
+  styleUrl: './tech-stack.component.scss',
 })
-export class TechStackComponent {
-
-}
+export class TechStackComponent {}
